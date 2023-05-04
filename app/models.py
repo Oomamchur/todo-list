7e2da3342fec1c3fdf6e573f35b5e8b65dd87744
Oleksandr Mamchur <irex1988@gmail.com>
2023-05-04 19:56:56 +0300

    implement all project

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["is_done", "datetime"]
+
+    def __str__(self) -> str:
+        return self.content
